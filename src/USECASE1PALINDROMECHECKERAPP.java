519662767dfd7315b1ce086ee752176b2ea3c38d
Prajukta Das <heyprajs@gmail.com>
2026-02-13 10:46:52 +0530

    INITIATE COMMIT

---
+++ src/USECASE1PALINDROMECHECKERAPP.java
@@ -0,0 +1,7 @@
+public class USECASE1PALINDROMECHECKERAPP {
+    public static void main(String[] args) {
+        System.out.println("Welcome top Palindrome Checker App");
+        System.out.println("Version 1.0");
+        System.out.println("System initialized successfully");
+    }
+}
