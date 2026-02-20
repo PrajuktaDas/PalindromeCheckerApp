b7b5cfeabd5e76206cf91a354bfa87a981bcac3a
PrajuktaDas <heyprajs@gmail.com>
2026-02-20 09:11:49 +0530

    added new feature

--- src/USECASE1PALINDROMECHECKERAPP.java
+++ src/USECASE1PALINDROMECHECKERAPP.java
@@ -1,27 +1,18 @@
-public class UseCase2PalindromeCheckerApp {
+public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-
         String input = "madam";
+        String reversed = "";
 
-
-        boolean isPalindrome = true;
-
-
-        for (int i = 0; i < input.length() / 2; i++) {
-
-
-            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
         }
 
+        System.out.println("Original String : " + input);
+        System.out.println("Reversed String : " + reversed);
 
-        System.out.println("Input text: " + input);
-
-        if (isPalindrome) {
+        if (input.equals(reversed)) {
             System.out.println("Is it a Palindrome? : true");
         } else {
             System.out.println("Is it a Palindrome? : false");
